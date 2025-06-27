7cd7cca4622f56518f25a2eeb5813188033cfc13
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-06-27 13:46:17 +0300

    Чистый код. Наследование + модификаторы доступа полей и методов при наследовании

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -3,410 +3,78 @@
 
 using namespace std;
 
-class TestClass;
-class Apple; 
-class HumanTakeApple;
-
-class Cap { //пример АГРЕГАЦИИ. класс не зависит от Human
-private:
-	string color = "Red";
-public:
-	string Get() {
-		return color;
-	}
-};
-
-class Model {
-private:
-	Cap cap;
-public:
-	void LookModel() {
-		cout << "Кепка модели " << cap.Get() << " цвета" << endl;
-	}
-};
-
 class Human {
-
-public: //нарушение инкапсуляции: объявление полей побличными
-	int age;
-	int weight;
-	string name;
-
-	void Print() { //используется для вывода установленных значений
-		age += 5;
-		cout << age << endl;
-		cout << weight << endl;
-		cout << name << endl;
-	}
-
-	void Think() {
-		brain.Think();//деллигирование (патерн проектирования)
-	}
-
-	void LookForCap(){
-		cout << "Моя кепка " << cap.Get() << " цвета" << endl;
-	}
-
 private:
-	
-
-	class Brain { // т.к. класс описан в private секции класса Human он служит только ему и вне/без него использоваться не может
-	public:		// пример КОМПОЗИЦИИ, жёсткая привязка класса к другому
-		void Think() {
-			cout << "Я думаю" << endl;
-		}
-	};
-
-	Brain brain;
-	Cap cap;
-};
-
-
-class Point {
-private:
-	int x;
-	int y;
-
+	string name = "Маша";
 public:
-
-	Point() { //улучшенный конструктор по умолчанию. в данном случае устанавливает 0 в значание переменной
-		x = 0;
-		y = 0;
-		cout << this << " constr" << endl;
+	string GetName() {
+		return name;
 	}
 
-	Point(int valueX, int valueY) { //конструктор с параметрами. принимает значения для установки в переменную изначально.
-		x = valueX;
-		y = valueY;
-		cout << this << " constr" << endl;
-	}
-
-	bool operator == (const Point& other) { // принимаем ссылку на другую пременную с которой сравниваем (чтобы видеть значиния)
-		return this->x == other.x && this->y == other.y; // возвращаем true когда все условия выполнились
-	}
-
-	bool operator != (const Point& other) {// принимаем ссылку на другую пременную с которой сравниваем (чтобы видеть значиния)
-		return !(this->x == other.x && this->y == other.y); //возвращаем true если условия не верны
-	}
-
-	Point operator -(const Point& other) {// принимаем ссылку на другую пременную с которой сравниваем (чтобы видеть значиния). Объект возвращает звачение типа класса
-		Point temp; // временная переменная для хранения результата
-		temp.x = this->x - other.x; // наша переменная - другая по параметрам
-		temp.y = this->y - other.y;
-		return temp; //возвращаем временную посчитанную переменную 
-	}
-
-	Point operator +(const Point& other) { //аналогично operator -
-		Point temp;
-		temp.x = this->x + other.x;
-		temp.y = this->y + other.y;
-		return temp;
-	}
-
-	Point operator *(const Point& other) {//аналогично operator -
-		Point temp;
-		temp.x = this->x * other.x;
-		temp.y = this->y * other.y;
-		return temp;
-	}
-
-	int GetX() { //получаем значение установленное в переменной, никаких параметров не передаём. Функция возвращает переменную того же типа
-		return x;
-	}
-
-	void SetX(int valueX) { //передаём в функцию значение которое и устанавливается в переменную. Функция ничего не возыращает
-		x = valueX;
-	}
-
-	int GetY() {//аналогично GetX()
-		return y;
-	}
-
-	void SetY(int y) { //аналогично SetX()
-		this->y = y;
-	}
-
-	void Print() {
-		cout << "x = " << x << endl;
-		cout << "y = " << y << endl << endl;
-	}
-
-	Point& operator ++() {  // инкремент префиксный (до переменной ++a)
-		this->x++;
-		this->y += 1;
-
-		return* this;
-	}
-
-	Point& operator --() { // декремент префиксный (до переменной ++a)
-		this->x--;
-		this->y--;
-
-		return*this;
-	}
-
-	Point& operator ++(int value) { // декремент постфиксный (после переменной a++). Параметр передаём только для того чтобф компилятор видел разницу
-		Point temp(*this); // храним значение во временной переменной чтобы увеличить сразу только его
-		this->x++;
-		this->y++;
-
-		return temp;
-	}
-
-	friend void chengeX(Point& value, TestClass& testValue); //дружественная функция получает доступ к приватным полям класса. не является членом класса
-};
-
-class TestClass {
-public:
-	int& operator[](int index) { //принимает номер ячейки данные которой нам нужны
-		return arr[index]; //возвращает значение лежащие в ячейке
-	}
-
-	friend void chengeX(Point& value, TestClass& testValue);
-
-private:
-	int arr[5] = { 1,2,3,4,5 };
-	int data = 0;
-};
-
-void chengeX(Point& value, TestClass& testValue) { //не является членом класса поэтому this не работатет. Может быть дружественной к нескольким классам
-	value.x = -1;
-	testValue.data = 99;
-
-}
-
-
-class CoffeGrinder {
-private:
-	bool ChekVoltage() { // метод для проверки напряжения, напряжение либо есть либо нет(поэтому булевый)
-		return true;
-	}
-
-
-public:
-	void Start() {
-		if (ChekVoltage()) //в зависимости от результата булевого метода выполняет определённое действие
-		{
-			cout << "Вжуууух" << endl;
-		}
-		else {
-			cout << "Напряжение не в норме. НЕ ВЖУХ(((" << endl;
-		}
-
-	}
-};
-
-class MyClass {
-private:
-	int* data;
-	int Size;
-
-public:
-
-	MyClass(int size) {
-		this->data = new int[size];
-		this->Size = size;
-
-		for (int i = 0; i < size; i++)
-		{
-			data[i] = i;
-		}
-		cout << data << " Вызвался конструктор " << this << endl;
-	}
-
-	MyClass(const MyClass& other) {
-
-		this->Size = other.Size;
-		this->data = new int[other.Size];
-		for (int i = 0; i < Size; i++)
-		{
-			this->data[i] = other.data[i];
-		}
-		cout << data << " Вызвался конструктор копирования" << this << endl;
-
-
-	}
-
-	~MyClass() {
-		cout << data << " Вызвался деструктор " << this << endl;
-		delete[] data;
-	}
-};
-
-
-void Foo(MyClass value) {
-	cout << "Вызвалась функция Foo" << endl; 
-}
-
-MyClass Foo2() {
-
-	cout << "Вызвалась функция Foo2" << endl;
-	MyClass temp(2);
-
-	return temp;
-}
-
-class Metody
-{
-public:
-	
-	void Print();
-
-private:
-
-};
-
-void Metody::Print() {
-	cout << "Сообщение" << endl;
-}
-
-class HumanTakeApple {
-public:
-	void TakeApple(Apple& apple);
-
-	void EatApple(Apple& apple) {
-		
-	}
-};
-
-class Apple {
-	friend HumanTakeApple; //дружественный класс
-private:
-	int weight;
-	string color;
-	string name;
-	int id;
-	static int count;
-
-	//friend void HumanTakeApple::TakeApple (Apple& apple); //дружественный метод
-public:
-	
-	Apple(int weight, string color, string name) {
-		this->color = color;
-		this->weight = weight;
+	void SetName(string name) {
 		this->name = name;
-		count++;
-		id = count; // оригинальные id равные количеству по созданию
 	}
-
-	int GetId() {
-		return id;
-	}
-
-	static int GetCount() { //статичный метод доступен всем, но ему не доступны нестатичные параметры
-		return count;
-	}
-
-	static void changeColor(Apple& apple, string color) {//статичный метод доступен всем, и ему доступны нестатичные параметры благодаря ссылке
-		apple.color = color;
-	}
-
 };
 
-int Apple::count = 0;
-
-void HumanTakeApple::TakeApple(Apple& apple)
-{
-	cout << "TakeApple: " << apple.name << endl << "weight: " << apple.weight << "\ncolor:" << apple.color<< endl << endl;
-}
-
-
-class Pixel {
-private:
-	int r;
-	int g;
-	int b;
-
-
+class Student: public Human {
 public:
+	string group;
 
-	Pixel() {
-		r = g = b = 0;
+	void Learn() {
+		cout << "Я учусь" << endl;
 	}
-	Pixel(int r, int g, int b) {
-		this->r = r;
-		this->g = g;
-		this->b = b;
-	}
-	string GetPixelInfo() {
-		return "Pixel: r=" + to_string(r) + " g= " + to_string(g) + " b= " + to_string(b);
-	}
+
 };
 
-
-class Image {
+class ZaoStudent: public Student {
 public:
-	void GetImageInfo() {
-		for (int i = 0; i < lenght; i++)
-		{
-			cout << pixels[i].GetPixelInfo() << endl;
-		}
+	void Learn() {
+		cout << "Я бываю в унике 1 в год" << endl;
 	}
-
-
-
-private:
-
-	static const int lenght = 5;
-	Pixel pixels[lenght]{
-		Pixel(0, 4, 64),
-		Pixel(4, 14, 10),
-		Pixel(111, 4, 24),
-		Pixel(244, 244, 14),
-		Pixel(111, 179, 64),
-	};
 };
+
+class Professor: public Human {
+public:
+	string subject;
+
+};
+
+class A {
+public:
+	string msg1 = "Сообщение 1";//можем обратиться везде, даже от объектов
+private:
+	string msg2 = "Сообщение 2"; //недоступны вне самого класса даже наследникам
+protected:
+	string msg3 = "Сообщение 3"; //доступны наследникам и самому классу, но не объектам
+};
+
+class B: public A { //public модификатор не меняетмодификаторов родителя. private-> все поля приватными. protected  public->protected 
+public:
+	void PrintMsg() {
+		
+		cout << msg1 << endl;
+	}
+};
+
 
 int main() {
 	setlocale(LC_ALL, "RU");
 
 
-	/*Human human;
-	human.Think();
-	human.LookForCap();
-	Model model;
-	model.LookModel();*/
+	B b;
+	b.PrintMsg();
 
-	/*MyClass a(10);
-	MyClass b(a);*/
+	/*Student st;
+	st.Learn();
 
-	/*Point a(1, 1);
-	Point b = a++;
-	++a;
-	Point c = a * b;*/
+	ZaoStudent zaoSt;
+	zaoSt.Learn();
 
-
-	/*TestClass a;
-	cout << a[2] << endl;*/
-	
-	/*TestClass test;
-	Point a(5, 12);
-	a.Print();
-	chengeX(a, test);
-	a.Print();*/
-
-	//Metody a;
-	//a.Print();
-
-	/*Apple apple(150, "Red", "first");
-	HumanTakeApple human;
-	human.TakeApple(apple);
-	Apple::changeColor(apple, "Gren");
-	human.TakeApple(apple);*/
-
-	/*HumanTakeApple human;
-	human.TakeApple(apple);
-	Apple apple2(50, "Green");
-	cout << Apple::GetCount();*/
-	//cout << "\ncountAllApple:" << apple.GetCount << endl;
-
-	//const int lenght = 5;
-	//Pixel arr[lenght];
-	//cout << arr[0].GetPixelInfo() << endl;
-	//arr[0] = Pixel(1, 1, 1);
-	//cout << arr[0].GetPixelInfo() << endl;
-	//Image pic;
-	//pic.GetImageInfo();
+	cout << st.GetName() << endl;
+	st.SetName("Саша");
+	cout<< st.GetName() << endl;
+	cout << "----" << endl;
+	cout<< zaoSt.GetName() << endl;*/
 
 
 	return 0;

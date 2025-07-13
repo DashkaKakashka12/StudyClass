c9c361d846c6aa835306ea71e96baeb929491d04
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-07-13 14:07:53 +0300

    auto_ptr = nique_ptr(уникальный). shared_ptr + умные указатели с массивами(0.5)

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 #include <windows.h>
-#include "StudyClass.h"
+#include "memory"
 
 using namespace std;
 
@@ -20,7 +20,7 @@ public:
 	}
 
 	~SmartPointer(){
-		delete a; //удаляет вксь объект и выделенную под него память тоже
+		delete a; //удаляет весь объект и выделенную под него память тоже
 		cout << "Destructor" << endl;
 
 	}
@@ -33,11 +33,26 @@ private:
 int main() {
 	setlocale(LC_ALL, "RU");
 
-	SmartPointer<int> a = new int(5); //не нужно очищать память. очищается с деструктором. 
-	//int* b = new int(3);
-	*a = 1234;
+	/*SmartPointer<int> a1 = new int(5);  
+	SmartPointer<int> a2 = a1;*/
 
-	cout << *a << endl;
+	/*auto_ptr<int> ap1(new int(5)); 
+	auto_ptr<int> ap2(ap1);*/ //указатель ap1 затирается и доступ остаётся только у ap2
+
+
+	//unique_ptr <int> ap1(new int(5)); 
+	//unique_ptr <int> ap2; //нельзя чтобы оба указателя ссылались на одну облась памяти
+	//ap2 = move(ap1); //p2.swap(p1) анфлог. передаём значение из ap1 в ap2, адрес ap1 не затирается(неопределённое состояние) 
+	//reset(затирает адрес и значение) release(затирает только значение)
+
+
+	/*shared_ptr <int> ap1(new int(5)); 
+	shared_ptr <int> ap2(ap1);*/ // оба указетеля могут цказывать на оду и туже облать памяти
+	//все данные будут уничтожены когда уничтожится последний указатель
+
+	int size;
+	int* arr = new int[size] { 1, 2, 3, 4, 5 };
+	shared_ptr <int[]> ap1(arr);
 
 
 	return 0;

60dbfe2bf9f556b19b5becfd25439341838ec8d3
DashkaKakashka12 <dashakarachun72@gmail.com>
2025-07-15 14:23:51 +0300

    prt2 (удалить первый, удаление всего списка и деструктор) + prt3 (добавление первого элемента, удаление последнего элемента, удаление элемента по индексу (см в отладке) )

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -14,6 +14,11 @@ public:
 
 	void push_back(T data);
 	int getSize() { return size; }
+	void pop_front();
+	void clear();
+	void push_front(T data);
+	void insert(T data, int index);
+	void removeAt(int index);
 
 	T& operator [](const int index);
 
@@ -48,7 +53,7 @@ List<T>::List() {
 
 template<typename T>
 List<T>::~List() {
-
+	clear();
 }
 
 template<typename T>
@@ -72,6 +77,82 @@ void List<T>::push_back(T data)
 }
 
 template<typename T>
+void List<T>::pop_front()
+{
+	Node<T> *temp = head; //обязательно указатель!!!!!
+	head = head->pNext;
+	delete temp;
+	size--;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+	while (size) {
+		pop_front();
+	}
+
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	head = new Node<T>(data, head);
+	/*Node<T>* current = this->head;
+	head = new Node<T>(data);
+	head->pNext = current; */
+	size++;
+
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+
+	if (index == 0) {
+		push_front(data);
+	}
+	else {
+
+		Node<T>* previous = this->head; //положили первый элемент
+		for (int i = 0; i < index - 1; i++) //итерируемся до элемента предшествующего нужному
+		{
+			previous = previous->pNext; //в наш первый добавляем индекс последующего элемента
+		}
+
+		Node<T>* newNode = new Node<T>(data, previous->pNext);
+		previous->pNext = newNode;
+
+		size++;
+	}
+
+
+	
+}
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+	if (index == 0) {
+		pop_front();
+	}
+	else {
+		Node<T>* previuos = this->head;
+
+		for (int i = 0; i < index - 1; i++) {
+			previuos = previuos->pNext;
+		}
+
+		Node<T>* toDelete = previuos->pNext;
+		previuos->pNext = toDelete->pNext; //я вообще не понимаю как это работает
+		delete toDelete;
+		size--;
+
+	}
+
+}
+
+template<typename T>
 T& List<T>::operator[](const int index)
 {
 	Node<T>* current = this->head;
@@ -79,10 +160,10 @@ T& List<T>::operator[](const int index)
 	int count = 0;
 
 	while (current != nullptr) { //идём пока адрес элемента следующего не равен нулю
+
 		if (count == index) {  
 			return current->data; // если количество равно принятому индексу возвращаем данные(тут значение)
 		}
-
 		current = current->pNext; // если количество не равно принятому индексу, заменяем объект следующим за ним по адресу и увеличиваем количество
 		count++;
 	}
@@ -94,17 +175,30 @@ int main() {
 	setlocale(LC_ALL, "RU");
 	
 	List<int> lst;
-	lst.push_back(5);
-	lst.push_back(10);
-	lst.push_back(40);
+	
+	lst.push_front(1);
+	lst.push_front(2);
+	lst.push_front(3);
+	lst.push_front(4);
 
-	cout << "Количество элементов в списке: "<< lst.getSize() << endl;
-
-	for (int i = 0; i < lst.getSize(); i++)
-	{
+	for (int i = 0; i < lst.getSize(); i++) {
 		cout << lst[i] << endl;
 	}
+	cout << "Количество элементов: " << lst.getSize() << endl;
 
+	lst.insert(55, 2);
+
+	for (int i = 0; i < lst.getSize(); i++) {
+		cout << lst[i] << endl;
+	}
+	cout << "Количество элементов: " << lst.getSize() << endl;
+
+
+	lst.removeAt(1);
+
+	for (int i = 0; i < lst.getSize(); i++) {
+		cout << lst[i] << endl;
+	}
 	
 	return 0;
 }

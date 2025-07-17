1b8ba49db42b29fc45ea5a9bf11b50eea00b7fb1
DashkaKakashka12 <dashakarachun72@gmail.com>
2025-07-17 17:11:13 +0300

    list -- реализация двухсвязного списка

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,78 +1,69 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 
 
 using namespace std;
 
-//итораторы == оболочка под умные указатели. используются для получения и изменения данных в динамических структурах
-//в разных дс разная реализация инераторов
+//list -- реализация двухсвязных списков
+//в листе не реализованы []. и нет доступа к индексу через итерации. только с начала или с конца по каждому элементу
 
-//в векторе есть и перегрузка[] и итераторы 
 
+//разница между постфиксным и префиксным. когда постфиксное мы создаём ещё один объект. допустим i++ где i = 5. мы вернём 5, но при этом i станет равно 6. префиксно мы сразу просто изменяем значение и возвращаем его
+//в итераторах это не влияет на логику работы, лишь на скорость. при ++i не нужно создавать ещё один объект
+
+
+template<typename T>
+void PrintList(const list<T>& lts) {
+	for (auto i = lts.cbegin(); i != lts.cend(); ++i)
+	{
+		cout << *i << endl;
+	}
+
+
+}
 
 int main() {
 	setlocale(LC_ALL, "RU");
 	
-	
-	vector<int> MyVector = {5, 6, 7, 8};
+	list<int> mylist = { 99,1,2,3,99,4,5,6 };
+	//mylist.push_back(111);
+	//mylist.push_front(222);
+	//mylist.pop_back();
+	//mylist.pop_front();
 
-	auto it = MyVector.begin();	 // begin возвращает данные нужного типа(vector тут). it предоставляет доступ к первому элементу vector
-	//*it = 1000; //можно заменить элемнет любой ячейки используя арифметику указателей
-	//it += 2; для передвижания по коллекции
-	//cout << *it << endl;
-	//advance(it, 2); //использыется для передвижания если нельзя через арифметику указателей
-
-	for (vector<int>::iterator i = MyVector.begin(); i != MyVector.end(); i++)
-	{
-		cout << *i << endl;
-	}
-	//end() указывает на следующий элемент после последнего(в никуда). const_iterator не позволяет менять значение
-	//для const_iterator работают и обычные begin, end по цепочке наследования. но можно cbegin, cend.
-	//reverse_iterator для перебора с конца. обязательно rbegin, rend. итераторы для хода назад. rend указывает на следующий за первым элементов. rbegin на последний эленет
+	PrintList(mylist);
+	cout << "количество элементов: " << mylist.size() << endl;
 
 	cout << endl << endl;
 
+	mylist.assign(5,1);	//заполняет лист 5 элементами. все значение равны 1
+
+	auto it = mylist.begin();
+	advance(it, 5); 
+	//mylist.insert(it, 0); 		//вставлюет элемент. не удаляет предыдущий а смещает их
+	//mylist.erase(it);		//удаляет элемент
 
 
+	//mylist.clear();		//очистка всего листа
+	//mylist.reverse();	//меняет порядок элементов от последнего к первому
+	//mylist.unique();	//удаление дубликатов(но только тех которые идут последоватьльно)
+	//mylist.remove(99); //удаляет все элеенты равные переданному значению
 
-	//МЕТОДЫ КОТОРЫЕ РАБОТАЮТ ТОЛЬКО С ИТЕРАТОРАМИ
-
-	advance(it, 3);
-	MyVector.insert(it, 1000);				//добавить элемент по какому-то индексу. если индекс не указывать то поставит на begin. advance для указания индекса
-	//удалить элемент по какому-то индексу
-
-
-	for (vector<int>::iterator i = MyVector.begin(); i != MyVector.end(); i++)
-	{
-		cout << *i << endl;
-	}
+	
+	PrintList(mylist);
+	cout << "количество элементов: " << mylist.size() << endl;
 
 
-	cout << endl << endl;
+	
+	
 
-	vector<int>::iterator itEr = MyVector.begin();
-	//advance(itEr, 3);
-	MyVector.erase(itEr); //тот элемент на который указывает итератор будет удалён из коллекции. itEr +3 удаляет диапазон
-
-
-	for (vector<int>::iterator i = MyVector.begin(); i != MyVector.end(); i++)
-	{
-		cout << *i << endl;
-	}
 	
 
 
-	/* Пример арифметики указателей на массиве
-	int arr[] = {1,2,3,};
-	cout << arr[1] << endl;
-	cout << *(arr + 1) << endl;*/ //адресс массива +1 элемент и разыменовать == второй элемент
+	//mylist.sort(); //сортируем от меньшего элемента к большему
 
-
-
-	//auto
-
-	//vector<int>::iterator it = MyVector.begin()	= 	auto it = MyVector.begin();
 
 
 	

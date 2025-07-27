f802a9d834d92191d98c430957b4af398b98c9b9
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-07-27 18:33:42 +0300

    array -- статический массив

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -3,47 +3,26 @@
 #include <vector>
 #include <list>
 #include <forward_list>
+#include <array>
 
 
 using namespace std;
 
-//forward_list -- реализация односвязного списка
-
-
-
+//array -- отличие от вектора в том что это обёртка для статического массива
 
 int main() {
 	setlocale(LC_ALL, "RU");
 	
-	forward_list<int> fl = {3, 4, 5};
-	//fl.push_front(4);
-
-	forward_list<int>::iterator it = fl.before_begin(); 
-
-	//fl.before_begin() как вставляет элемент в начало. можно просто push front
+	array<int, 4> arr = {2, 3, 4, 5}; //обязательно вторым параметром указатькол-во эл-тов
 	
+	cout << arr.back() << endl;
 
 
-	//fl.erase_after(it); //удаляет СЛЕДУЮЩИЙ за указанным элемент
-	//fl.insert_after(it, 9999); //вставить элемент перед каким-то
-	//it--; не хранят элементы адрес предыдущего, только следующего, поэтому декремента нет
-	//cout << *it << endl;
-
-	for (auto el : fl) {
-		cout << el << endl;
-	}
-
-	//size -- размерность
-	//clear --очистить
-	//insert -- вставить элемент
-	//erase --  
-	//pop -- удалить
-	//push -- добавить
-	//front -начало(вершина)
-	//back -- конец
-	//assign(5,1) --заполняет лист 5 элементами. все значение равны 1
-
-	
+	//arr.at(0) -- проверяет выход за границы размера
+	//arr.fill(2); //заполняет все элементы одним значением
+	//arr.back() -- вывод последнего эл-та
+	//arr.front() --вывод первого эл-та
+	//arr.size -- узнать размер
 
 
 	return 0;

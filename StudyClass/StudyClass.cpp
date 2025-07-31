f8d1c042767930480247356410141971c0ca3cc3
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-07-31 14:12:41 +0300

    union

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,42 +1,27 @@
 ﻿#include <iostream>
-#include <thread>
-#include <chrono>
-
 
 
 using namespace std;
 
-//возврат параметров из потока по ссылке с помощью std::ref(q)
-//std::ref(q) --создание класса который позволяет работать с потоком который позволяет работать с реузльтатом как ссылкой
+//union -- объединение -- технология которая позвооляет делить одну и ту же область памяти под разные данные
+//union выделяет место для самой большой тип данных, если используется меньший, то свободное выделенное место забивается нулями
 
-void DoWork(int &a) {
 
-	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "ID: " << this_thread::get_id() << "----------DoWork START" << endl;
-	this_thread::sleep_for(chrono::milliseconds(2000));
-
-	a *= 2;
-	
-	cout << "ID: " << this_thread::get_id() << "----------DoWork STOR" << endl;
-}
-
+union My { //данный union выделяет 4 байта
+	short int a; //2 байта
+	int b; //4 байта
+	float c; //4 байта
+};
 
 int main() {
 	setlocale(LC_ALL, "RU");
 
-	int q = 2;
-	//DoWork(q);
-	thread th(DoWork, std::ref(q));	//параметры передаются через запятую вместе с ссылкой на функцию
+	My m;
+	m.a = 5; //когда мы в union запишем другое значение она перезапишет все значения 
+	m.b = 2000;
+	m.c = 43.56;
 
-	for (size_t i = 0; i < 15; i++)
-	{
-		cout << "ID: " << this_thread::get_id() << "\tmain\t" << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(500)); //делает паузу благодаря библиотеке chrono в 1 сек
-	}
-
-	th.join(); //если использовать detach то есть верояятность что поток недовыполнится и тогда резульат вернётся неккоректным
-	cout << "q = " << q << endl;
-
+	cout << m.c << endl;
 
 	return 0;
 }

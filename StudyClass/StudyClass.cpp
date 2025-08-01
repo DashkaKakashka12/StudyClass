7e0a3cb3da07d8841fb1796aef7402d6ccd0c4e9
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-08-01 14:22:17 +0300

    Многопоточное программирование. Получение результата работы из потока

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,81 +1,41 @@
 ﻿#include <iostream>
-#include <functional>
-#include <vector>
+#include <thread>
+#include <chrono>
 
 
 using namespace std;
 
-//захват переменных в контекст [] лямбда функций
-//переменные в [] передаются через запятую,
-//[=] - ситывание всего контекста до лямбды по значению
-//[&] - ситывание всего контекста до лямбды по ссылке
-//mutable ключевое слово при захвате по значению позваляет изменять переданные параметры, но только внутри лямбды(работать с копией) 
-//[&a,b]() mutable -- с а работаем как с ссылкой, с b со значением
+//получение результата рабоыт из потока
+//чтобы получить результат который возвращает функция надо объявить при создании потока лямбда функцию, переменная в которой получит результат работы первой функции 
+//thread th([&res](){ res = Sum(2, 3); } );
 
+int Sum(int a, int b) {
 
-void Foo(int a) {
-	if (a > 10 && a < 40) {
-		cout << "Foo: ";
-		cout << a << endl;
-	}
+    this_thread::sleep_for(chrono::milliseconds(3000));
+    cout << "---Sum START---" << endl;
+    this_thread::sleep_for(chrono::milliseconds(7000));
+    cout << "---Sum STOP---" << endl;
+
+    return a + b;
 }
-
-
-void Bar(int a) {
-	if (a % 2 == 0) {
-		cout << "Bar: ";
-		cout << a << endl;
-	}
-}
-
-void DoWork(vector<int>& vc, function<void(int)> f) { //function<void(int,int)> f
-	//vector<function<void(int)>> fvc -- вектор из всех функций с соответствующей сигнатурой
-	for (auto el : vc) {
-		f(el);
-	}
-
-}
-
-class MyClass
-{
-public:
-	int a = 30;
-	MyClass() {};
-	~MyClass() {};
-
-	void Msg() {
-		cout << "msg" << endl;
-	}
-
-	void L() {
-		auto f = [this]() { //позврляет использовать this внутри лямбды
-			this->Msg();
-		};
-		f();
-	};
-
-
-};
 
 
 int main() {
-	setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "RU");
 
-	MyClass m;
-	m.L();
+    int res;
 
-	int a = 55;
-	int b = 10;
+    thread th([&res](){ res = Sum(2, 3); } );
 
-	auto f = [&a,b]() mutable{ //для указания типа возвращаемого значения помимо явного можно использовать f = [&a,b]()-> int
-		cout << a << endl;
-		a = 1;
-		b = 1;
-		cout << b << endl;
-		return 0;
-	};
 
-	auto result = f();
+    for (size_t i = 0; i<=10; i++)
+    {
+        cout << "ID: " << this_thread::get_id() << "\tmain\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500)); //делает паузу благодаря библиотеке chrono в 1 сек
+    }
 
-	return 0;
+   th.join();
+   cout <<"res: "<< res << endl;
+
+    return 0;
 }

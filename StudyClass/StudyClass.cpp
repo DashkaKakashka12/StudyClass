6892305cf0f46baea907c6300c46e2fda9fa04b3
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-08-02 17:16:35 +0300

    функторы

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,71 +1,29 @@
 ﻿#include <iostream>
-#include <thread>
-#include <chrono>
-
-
 using namespace std;
 
-//время выполнения участка кода
-//chrono можно использовать дял измерения времени участков кода без потоком. по умолчанию в thread подключена chrono
+//функторы -- функциональные объекты. объекты класса у которых перегружен оператор (), из-за чего может вести себя как функция
+//может использоваться для подсчёта count.
 
-//для участка кода
-/*auto start = chrono::high_resolution_clock::now(); -- в начале выполнения
-auto end = chrono::high_resolution_clock::now(); -- в конце выполнения
-chrono::duration<float> duration = end - start;
-cout << "duration: " << duration.count() << " s" << endl;*/
-
-class SimpleTimer
-{
+class MyFunctor {
 public:
-    SimpleTimer() {
-        start = std::chrono::high_resolution_clock::now();
-    };
-    ~SimpleTimer() {
-        end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> duration = end - start;
-        cout << "duration: " << duration.count() << " s" << endl;
-    };
+    int operator() (int a, int b) 
+    { //operstor() -- вместо названия объекта, так как он может принимать парамтры
+        cout << "я функтор " <<endl;
+        return a+b;
+    }
 
 private:
-    std::chrono::steady_clock::time_point start, end;
-
+    int count = 0;
 };
 
 
-int Sum(int a, int b) {
-    SimpleTimer st;
-    this_thread::sleep_for(chrono::milliseconds(3000));
-    cout << "---Sum START---" << endl;
-    this_thread::sleep_for(chrono::milliseconds(7000));
-    cout << "---Sum STOP---" << endl;
-
-    return a + b;
-}
 
 int main() {
     setlocale(LC_ALL, "RU");
+    MyFunctor f;
+    int result = f(2, 2);
+    cout <<result;
 
-    SimpleTimer st;
-
-    //auto start = chrono::high_resolution_clock::now();
-    //int a = Sum(2, 3); //15.6205 s, без задержви во времени  5.62594 s
-    int res;
-
-    thread th([&res](){ res = Sum(2, 3); } ); //10.0235 s
-
-    for (size_t i = 0; i<=10; i++)
-    {
-        cout << "ID: " << this_thread::get_id() << "\tmain\t" << i << endl;
-        this_thread::sleep_for(chrono::milliseconds(500)); 
-    }
-    
-   th.join();
-    cout <<"res: "<< res << endl;
-
-
-    //auto end = chrono::high_resolution_clock::now();
-   // chrono::duration<float> duration = end - start;
-   // cout << "duration: " << duration.count() << " s" << endl;
 
     return 0;
 }

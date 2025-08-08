4d60939473e20b34514c381cd051408af1b36417
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-08-08 17:45:38 +0300

    unique -- удаления повторяющихся элементов, uniqu_copy(+копирование)

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,31 +1,34 @@
 ﻿#include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-//алгоритм for_each -- функция для перебора коллекции и получения доступ к каждому элементу
-//третий параметр функция или лямбда. если лямбда то можно не прописывать элементарные функции
-//и алгоритм и цифк принимают параметры по значению, если надо по ссылке, то в указываем по ссылке
+//алгоритм unique -- для удаления повторяющихся элементов. как remove, не удаляет а преносит элементы в конец не меняя кол-ва элементов коллекции
+//для вывода эл-тов до границы используем for_each, для удаления ненужных элементов используем erase(result, v.end())
 
-//разницы в результате работы для вывода между циклом и алгоритмом нет
+//uniqu_copy -- позволяет скопировать уникальные элементы в новый контецнер
+//не обязательно чтобы два контейнер абыли одинаковы для uniqu_copy, важен тип данных
 
-void Foo(int a) {
-   
-    cout << a << endl;
-}
 
 
 int main() {
     setlocale(LC_ALL, "RU");
 
-    int arr[] = {1,2,7,4,};
-  
-    for_each(begin(arr), end(arr), [](int a) {
-        cout << a << endl;});
+    vector<int> arr = {1,2,7,4,4,7,8,1};
+    vector<int> arr2;
 
-    /*for (auto el : arr) {
-        Foo(el);
-    }*/
+    for (auto el : arr) {
+        cout << el << " ";
+    }
+
+    cout << endl;
+    //auto result = unique(begin(arr), end(arr));
+    auto result = unique_copy(begin(arr), end(arr), back_inserter(arr2));
+    for_each(begin(arr2), end(arr2), [](int a) {cout << a << " ";});
+    //for_each(begin(arr), result, [](int a) {cout << a << " ";});
+    
+   
 
     return 0;
 }

055a2d1a810a835b9367cc90836cd9412014a60d
DashkaKakashka12 <daryakarachun72@gmail.com>
2025-08-09 18:03:05 +0300

    unique_lock -- расширенный lock_guard для улучшенной работы mutex

--- StudyClass/StudyClass.cpp
+++ StudyClass/StudyClass.cpp
@@ -1,34 +1,52 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include "SimpleTimer.h"
 
 using namespace std;
 
-//алгоритм random_shuffle -- располагает элементы в рандомном порядке
-//для генерации уникальных порядков использовать random_shuffle
+//вспомни lock_guard<mutex> lg(m) ограничивает доступ к функции если к ней уже обращён один поток
+//unique_lock -- работает также как и lock_guard, но есть методы, которые позволяет ограничить часть кода без {}, а с unlock
+//используется вместо lock_guard когда есть часть кода внутри которая может быть выполнена двумя и более потоками одновременно
 
 
-//позволяет выполнить переопределённый метод наследников класса в рандомном порядке
+mutex m;
+void Print(char ch) {
+    
+        //lock_guard<mutex> lg(m);
+
+        unique_lock<mutex> ul(m, defer_lock); //defer_lock не вызовет метод lock при создании. надо прописать самим там где нужно
+
+        this_thread::sleep_for(chrono::milliseconds(2000));
+        ul.lock();
+        for (int i = 0; i < 5; i++)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                cout << ch;
+                this_thread::sleep_for(chrono::milliseconds(10));
+            }
+            cout << endl;
+        }
+        cout << endl;
+        ul.unlock();
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+}
 
 
 
 int main() {
     setlocale(LC_ALL, "RU");
-    srand(time(NULL));
 
-    int arr[] = {1,2,3, 4,5};
+    SimpleTimer t;
 
-    for (auto el : arr) {
-        cout << el << " ";
-    }
+    thread t1(Print, '*');
+    thread t2(Print, '-');
 
-    cout << endl;
+    t1.join();
+    t2.join();
 
-    random_shuffle(begin(arr), end(arr));
-   
-    for (auto el : arr) {
-        cout << el << " ";
-    }
-    
     return 0;
 }
